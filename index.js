8d643a66e974d227432e0b41254c0492f3086639
Rubanrubi <keerthi@sparkouttech.com>
2020-02-02 11:23:46 +0530

    Imported and used chalk library

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const validator = require('validator')
 const add = require('./add.js')
 const fs = require('fs')
+const chalk = require('chalk')
 
 var sum = add(1, 2)
 
@@ -11,3 +12,9 @@ fs.writeFileSync('ruban.txt', 'My Name is Ruban')
 console.log(validator.isEmail('ruban@gmail.com'));
 
 console.log(validator.isUppercase('RUBAN'));
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
